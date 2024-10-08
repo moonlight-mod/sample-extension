fd9703886bb8f24a6990ad544c690db5fcb842e8
Luna <git@l4.pm>
2024-10-08 16:30:17 -0300

    fix redispatcher logic

--- src/dmrf/node.ts
+++ src/dmrf/node.ts
@@ -4,54 +4,62 @@ import type { DMRFNatives, MRF } from "./types";
 let loaded_mrfs: MRF[] = [];
 
 function init() {
-  let scripts = moonlightNode.getConfigOption<string[]>("dmrf", "mrfScripts") ?? [];
+  let scripts =
+    moonlightNode.getConfigOption<string[]>("dmrf", "mrfScripts") ?? [];
   loaded_mrfs = scripts.map((scriptPath) => {
+    console.log("[dmrf] Loading MRF: ", scriptPath);
     const mrf = require(scriptPath);
     if (mrf.init) {
       mrf.init();
     }
     return mrf;
-  })
+  });
 }
 
-async function receiveHook(msg: any): boolean {
-  for(const mrf of loaded_mrfs) {
+async function receiveHook(msg: any): Promise<boolean> {
+  for (const mrf of loaded_mrfs) {
     let rejectFunction = (_: any) => {
-      return false
-    }
+      return false;
+    };
 
     let forwardFunction = (_: any) => {
-      return true
-    }
+      return true;
+    };
 
-    const forwardMessage = mrf.receiveHook(msg, rejectFunction, forwardFunction)
-    if (!forwardMessage) {
-      return false
+    if (mrf.receiveHook) {
+      const forwardMessage = mrf.receiveHook(
+        msg,
+        rejectFunction,
+        forwardFunction
+      );
+      if (!forwardMessage) {
+        return false;
+      }
     }
   }
   return true;
 }
 
-function sendHook(msg: any): boolean {
-  for(const mrf of loaded_mrfs) {
+async function sendHook(msg: any): Promise<boolean> {
+  for (const mrf of loaded_mrfs) {
     let forwardFunction = (_: any) => {
-      return true
-    }
+      return true;
+    };
 
-    const forwardMessage = mrf.sendHook(msg, forwardFunction)
-    if (!forwardMessage) {
-      return false
+    if (mrf.sendHook) {
+      const forwardMessage = mrf.sendHook(msg, forwardFunction);
+      if (!forwardMessage) {
+        return false;
+      }
     }
   }
   return true;
 }
-
 
 const dmrf_exports: DMRFNatives = {
   init,
   receiveHook,
-  sendHook,
+  sendHook
 };
 
 module.exports = dmrf_exports;
-
--- src/dmrf/webpackModules/dmrf.ts
+++ src/dmrf/webpackModules/dmrf.ts
@@ -1,14 +1,18 @@
 import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
 import Dispatcher from "@moonlight-mod/wp/discord/Dispatcher";
 import { DMRFNatives } from "../types";
+import { LogLevel } from "@moonlight-mod/types";
 
 const COOL = "Queueing message to be sent";
 const module = spacepack.findByCode(COOL)[0].exports;
 
+const natives: DMRFNatives = moonlight.getNatives("dmrf");
+
+natives.init();
+
 const originalSend = module.Z.sendMessage;
 module.Z.sendMessage = async (...args: any[]) => {
   const message = args[1];
-  //natives.sendHook(message);
   //args[1].content = args[1].content.replace('https://x.com','https://fixfx.com');
   //args[1].content = args[1].content.replace('http://x.com','http://fixfx.com');
   await natives.sendHook(message);
@@ -16,24 +20,26 @@ module.Z.sendMessage = async (...args: any[]) => {
   return originalSend.call(module.Z, ...args);
 };
 
-const natives: DMRFNatives = moonlight.getNatives("dmrf");
-
-natives.init();
-
 async function reDispatcher(event: any) {
+  console.log("redispatch");
   // set all message content in messages array
   // NOTE thank u husky
-  if (event.hasOwnProperty("messages") && Array.isArray(event.messages)) {
-    event.messages.forEach((message: any) => {
-      if (
-        message.hasOwnProperty("content") &&
-        typeof message.content === "string"
-      ) {
-        const drop = natives.receiveHook(message);
-        event.dmrf = true;
-        if (!drop) Dispatcher.dispatch(event);
-      }
-    });
+  if (event.type === "LOAD_MESSAGES_SUCCESS") {
+    if (event.hasOwnProperty("messages") && Array.isArray(event.messages)) {
+      event.messages.filter((message: any) => {
+        if (
+          message.hasOwnProperty("content") &&
+          typeof message.content === "string"
+        ) {
+          return !natives.receiveHook(message);
+          //event.dmrf = true;
+          //console.log("drop", drop, event.type);
+          //if (!drop) Dispatcher.dispatch(event);
+        }
+      });
+      event.dmrf = true;
+      if (event.messages) Dispatcher.dispatch(event);
+    }
   } else if (event.type == "MESSAGE_CREATE") {
     const drop = natives.receiveHook(event.message);
     event.dmrf = true;
@@ -44,7 +50,7 @@ async function reDispatcher(event: any) {
 }
 
 Dispatcher.addInterceptor((event) => {
-  if (event.dmrfDispatch) return false;
+  if (event.dmrf) return false;
 
   if (event.type === "LOAD_MESSAGES_SUCCESS") {
     reDispatcher(event);
